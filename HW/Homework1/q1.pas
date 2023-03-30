81b89a1fcc0366e8e1eb4dea11f0211f02aaa0b5
MichalOz <michalozeri55@gmail.com>
2023-03-30 16:16:10 +0300

    q1 fixed

--- HW/Homework1/q1.pas
+++ HW/Homework1/q1.pas
@@ -1,10 +1,26 @@
 program PascalTriangle;
+procedure makeLine(var prevLine: array of integer; n: integer);
+var left : integer;
+var update : integer;
+var i : integer;
+begin
+    left := 0;
+    for i := 1 to (n-1) do
+    begin
+        update := left + prevLine[i];
+        left := prevLine[i];
+        prevLine[i] := update;
+        Write(prevLine[i], ' ')
+    end;
+    prevLine[n] := 1;
+    WriteLn(prevLine[n])
+end;
 {
     Here you may write helper functions and procedures.
     Please do.
 }
 
-var n,i: integer;
+var n, i: integer;
 var line: array [1..100] of integer; { Complete the array definition... }
 
 begin
@@ -17,5 +33,5 @@ for i := 1 to n do
         {
             Construct the new line and then print it.
         }
-    end
+    end;
 end.
